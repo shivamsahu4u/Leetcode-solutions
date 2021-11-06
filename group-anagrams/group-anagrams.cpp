68a28f9aec31adeca7b1124607328ea267a82eeb
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2021-11-07 00:10:35 +0530

    Time: 96 ms (13.75%), Space: 37.7 MB (5.02%) - LeetHub

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -2,7 +2,35 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-         unordered_map<string, vector<string>>hash;
+        //we can improve time complexity by using not using sort
+        
+      map<map<char, int> , vector<string>>hash;
+        vector<vector<string>>ans;
+        
+        int n = strs.size();
+        
+        for(int i = 0 ; i < n ; i++){
+            
+              string ans = strs[i];
+              map<char , int>press;
+              for(int j = 0 ; j < ans.size() ; j++){
+                  press[ans[j] - 'a']++;
+              }
+            
+            hash[press].push_back(strs[i]);
+        }
+        
+        for(auto  it = hash.begin() ; it !=hash.end() ; it++){
+            ans.push_back(it->second);
+        }
+        
+        return ans;
+        
+        
+        // -- Time Complexity is O(n*k*log(k)) where n is length of strs and k is length of string
+        // -- Space Complexity is O(n) 
+        
+       /*  unordered_map<string, vector<string>>hash;
         vector<vector<string>>res;
         for(int i = 0 ; i < strs.size() ; i++){
             
@@ -16,6 +44,6 @@ public:
             res.push_back(it->second);
         }
         
-        return res;
+        return res;*/
     }
 };
