06205c825068355cc9cff854d1dcbe4f7631476a
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2021-11-06 22:10:57 +0530

    update all the anagrams in a string , updated

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string name1, string name2) {
+    
+    // -- Time complexity is O(n*26)
+    // -- Space complexity is O(26) + o(26)
+    vector<int> findAnagrams(string s, string p) {
+	vector<int> goal(26), cur(26), res;
+    //since it will contain only characters , so delaraing the vector of characters    
+        
+	for(char c : p) goal[c - 'a']++;
+    //putting the characters in the goal vector
+    
+	for(int i = 0; i < s.size(); i++) {
+		cur[s[i] - 'a']++;
+		if(i >= p.size()) cur[s[i - p.size()] - 'a']--;
+		
+        //here we are comparing two vectors with its values , so its O(26)
+        if(cur == goal) res.push_back(i - p.size() + 1);
+	}
+        
+        
+	return res;
+}
+    
+    
+   /* vector<int> findAnagrams(string name1, string name2) {
         
        vector<int>ans;
 	int n = name1.size();
@@ -60,5 +83,5 @@ public:
 
 	return ans;
 
-    }
+    }*/
 };
