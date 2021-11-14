2e110296ff2cf0cdb1a0e2274c656e0e370048a8
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2021-11-14 23:58:06 +0530

    Time: 4 ms (86.70%), Space: 15.1 MB (16.49%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -10,49 +10,62 @@
 class Solution {
 public:
     
-   vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
-        unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
-        unordered_map<TreeNode*, bool> visited;
-        queue<TreeNode*> queue;
-        queue.push(root);
-        while(!queue.empty()) { /*First BFS to get a track of parent nodes*/
-            TreeNode* current = queue.front(); queue.pop();
-            if(current->left) {
-                parent_track[current->left] = current;
-                queue.push(current->left);
-            }
-            if(current->right) {
-                parent_track[current->right] = current;
-                queue.push(current->right);
-            }
-        }
-        queue.push(target);
-        visited[target] = true;
-        int curr_level = 0;
-        while(!queue.empty()) { /*Second BFS to go upto K level from target node and using our hashtable info*/
-            int size = queue.size();
-            if(curr_level++ == K) break;
-            for(int i=0; i<size; i++) {
-                TreeNode* current = queue.front(); queue.pop();
-                if(current->left && !visited[current->left]) {
-                    queue.push(current->left);
-                    visited[current->left] = true;
-                }
-                if(current->right && !visited[current->right]) {
-                    queue.push(current->right);
-                    visited[current->right] = true;
-                }
-                if(parent_track[current] && !visited[parent_track[current]]) {
-                    queue.push(parent_track[current]);
-                    visited[parent_track[current]] = true;
-                }
-            }
-        }
-        vector<int> result;
-        while(!queue.empty()) {
-            TreeNode* current = queue.front(); queue.pop();
-            result.push_back(current->val);
-        }
-        return result;
-    }
+//k distance nodes from the target 
+//Recursion Method
+
+unordered_map<TreeNode* , TreeNode*>parent; //parent of node
+unordered_map<TreeNode* , bool>visited; // node is visited or not
+vector<int>ans; // storage of answer
+void par(TreeNode* root){
+
+     if(root == NULL){
+        return;
+     }
+
+     if(root->left){
+        parent[root->left] = root;
+        par(root->left);
+     }
+
+     if(root->right){
+        parent[root->right] = root;
+        par(root->right);
+     }
+}
+vector<int> distanceK(TreeNode* root, TreeNode* target, int K){
+
+     if(root == NULL)return ans;
+     par(root);
+     dfs(target , K);
+     return ans;
+}
+void dfs(TreeNode* target , int k){
+
+     if(visited.find(target) != visited.end()){
+        return ;
+     }
+
+     visited[target] = true;
+
+     if(k == 0){
+        ans.push_back(target->val);
+        return;
+     }
+
+     if(target->left){
+        dfs(target->left , k-1);
+     }
+
+     if(target->right){
+        dfs(target->right , k-1);
+     }
+
+     TreeNode*p = parent[target];
+
+     if(p){
+        dfs(p , k-1);
+     }
+}
+
+
 };
