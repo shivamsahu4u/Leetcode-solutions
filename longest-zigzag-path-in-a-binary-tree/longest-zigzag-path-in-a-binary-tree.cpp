5df131fcb8abe2b28d218e38ac6987a2aa9c674d
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2021-11-17 00:13:03 +0530

    Time: 132 ms (97.18%), Space: 94.2 MB (84.51%) - LeetHub

--- longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    int ans = 0;
+   /* int ans = 0;
    void inorder(TreeNode* root,bool dir,int len){
         
         if(root==NULL) return ;
          
         ans=max(ans,len);
-        if(dir==0){
-            inorder(root->left,1,len+1);
+        if(dir==0){ //root is right
+            inorder(root->left,1,len+1);//root will move to left
             inorder(root->right,0,1);
         }
-        else{
-             inorder(root->right,0,len+1);
+        else{ //root is left
+             inorder(root->right,0,len+1); // root will move to right
              inorder(root->left,1,1);
         }
          
@@ -31,9 +31,66 @@ public:
     
     int longestZigZag(TreeNode* root) {
         ans=0;
-         inorder(root->left,1, 1);
-         inorder(root->right,0, 1);
+         inorder(root->left,1, 1); // root is left
+         inorder(root->right,0, 1);// root is right
         
         return ans ;
+    }*/
+    
+/*int ans;
+void helper(TreeNode*root , bool s , int d){
+
+    if(root == NULL) return;
+
+    ans = max(ans , d);
+
+    if(s == 0){
+        helper(root->left , 1 , d+1);
+        helper(root->right , 0 , 1);
+    }else{
+        //root is at right
+        //means we need to move left
+        helper(root->right , 0 , d+1);
+        helper(root->left , 1 , 1);
     }
+    return;
+}
+int longestZigZag(TreeNode*root){
+  ans= 0;
+   helper(root->left , 1 , 1); 
+   //1 means root is at left
+  
+   helper(root->right , 0 , 1);
+   //0 means root is at right 
+    return ans;
+}*/
+    
+ //Time Complexity -- O(n)
+    //Space Compleity is -- O(height)
+int ans = 0;
+void helper(TreeNode*root , bool s , int d){
+
+    if(root == NULL) return;
+
+    ans = max(ans , d);
+
+    if(s == 0){
+        helper(root->left , 0 , 1);
+        helper(root->right , 1 , d+1);
+    }else{
+        //root is at right
+        //means we need to move left
+        helper(root->left , 0 , d+1);
+        helper(root->right , 1 , 1);
+    }
+    return;
+}
+int longestZigZag(TreeNode*root){
+
+   helper(root->left , 0 , 1); 
+   //0 means root is at left
+   helper(root->right , 1 , 1);
+   //1 means root is at right 
+    return ans;
+}
 };
