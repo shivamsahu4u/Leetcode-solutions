f7dfb913cc9f7fdf7c6a81eee697843027c39f9b
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2021-12-03 16:08:02 +0530

    Time: 140 ms (57.20%), Space: 84.5 MB (78.25%) - LeetHub

--- all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -12,7 +12,50 @@
 class Solution {
 public:
     
-    vector<int>ans , res;
+     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+         
+         stack<TreeNode*>s1 , s2;
+         vector<int>ans;
+         TreeNode*temp1 = root1 , *temp2 = root2;
+          while(temp1!=NULL){
+              s1.push(temp1);
+              temp1 = temp1->left;
+          }
+         while(temp2!=NULL){
+             s2.push(temp2);
+             temp2=temp2->left;
+         }
+         while(!s1.empty() || !s2.empty()){
+             
+             if(!s1.empty() && (s2.empty() || s1.top()->val < s2.top()->val)){
+                 ans.push_back(s1.top()->val);
+                 TreeNode*k = s1.top();
+                 s1.pop();
+                 
+                 if(k->right!=NULL){
+                     TreeNode*temp = k->right;
+                     while(temp!=NULL){
+                         s1.push(temp);
+                         temp = temp->left;
+                     }
+                 }
+             }else{
+                 ans.push_back(s2.top()->val);
+                 TreeNode*k = s2.top();
+                 s2.pop();
+                 
+                 if(k->right!=NULL){
+                     TreeNode*temp = k->right;
+                     while(temp!=NULL){
+                         s2.push(temp);
+                         temp = temp->left;
+                     }
+                 }
+             }
+         }
+         return ans;
+     }
+  /*  vector<int>ans , res;
     void helper(TreeNode* root){
         if(root == NULL) return ;
         helper(root->left);
@@ -21,16 +64,15 @@ public:
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
       
-          helper(root1);
-          helper(root2);
+          helper(root1);   // O(no. of nodes say m)
+          helper(root2); // O(no. of nodes say n)
         for(int i = 0 ; i < res.size() ; i++){
             ans.push_back(res[i]);
-        }
+        }  // O(min(m,n))
         
-       sort(ans.begin() , ans.end());
-        
-        
+       sort(ans.begin() , ans.end());  
+      //we can use two pointer approach here  
         
         return ans;
-    }
+    }*/
 };
