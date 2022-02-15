a236b254c8bcd034b2270b133a166953003867ee
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-02-16 00:32:59 +0530

    Time: 397 ms (19.90%), Space: 69.1 MB (31.76%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,108 +1,43 @@
 class Solution {
 public:
-    // We cant use dfs as it will give tle as we visit on the paths
-    double mx = 0.0;
-    
-    void dfs(int u , int v , vector<pair<int , double>>adj[] , vector<bool>&visited , double a){
-         cout<<"sfsf";
-        if(u == v){
-             cout<<"sfsf";
-            if(mx < a){
-                cout<<"sfsf";
-                mx = a;
-            }
-            return ;
-        }
-        visited[u] = true;
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         
-        for(auto it : adj[u]){
+        //we can use max priority queue for finding maximum distance between any two nodes
+        
+        vector<pair<int,double>>arr[n];
+        int i = 0;
+        for(auto path : edges){
             
-          if(!visited[it.first]){
+            arr[path[0]].push_back(make_pair(path[1] , succProb[i]));
+            arr[path[1]].push_back(make_pair(path[0] , succProb[i]));
+            i++;
+        }
+        
+        priority_queue<pair<double , int>>q;
+        q.push(make_pair( 1.0 , start));
+        vector<double>ans(n , 0);
+        ans[start] = 1;
+      
+        
+        vector<bool>visited(n ,false);
+        while(q.size() != 0){
+           
+                
+                pair<double , int>front = q.top();
+                q.pop();
               
-              double t = a*it.second;
-              dfs(it.first , v , adj ,visited ,t);
-          }
-        }
-        
-        visited[u] = false;
-    }
-//     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        
-//          //we can use simple dfs for this
-//         //time complexity - O(n) since we visit only n nodes at once
-        
-//         vector<pair<int , double>>adj[n];
-//         int i = 0;
-//         for(auto path : edges){
-//             adj[path[0]].push_back(make_pair(path[1] , succProb[i]));
-//            adj[path[1]].push_back(make_pair(path[0] , succProb[i]));
-//             i++;
-//         }
-        
-//         vector<bool>visited(n , false);
-        
-//      //   dfs(start , end , adj , visited , 1.0);
-       
-        
-//         queue<pair<int , double>>q;
-//         q.push(make_pair(start , 1.0));
-        
-//         double mx = 0.0;
-//         while(q.size()!=0){
-            
-            
                 
-//                 pair<int , double> front = q.front();
-//                 q.pop();
-//                 //visited[front.first] = true;
-//                 if(front.first == end){
+                for(auto it : arr[front.second]){
                     
-//                      if(mx < front.second){
-//                          mx = front.second;
-//                      }
-//                 }
-                
-//                 for(auto it : adj[front.first]){
-                    
-//                    // if(!visited[it.first])
-//                     q.push(make_pair(it.first , it.second*front.second));
-//                 }
-//                 //visited[front.first] = false;
+                  
+                    if(front.first*it.second > ans[it.first]){
+                      
+                        ans[it.first] = front.first*it.second;
+                        q.push(make_pair(ans[it.first] , it.first));
+                    }
+                }
             
-//         }
-        
-//         return mx;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//     }
-     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-    vector<vector<pair<int, double>>> to(n);
-        for(int i=0;i<edges.size();i++){
-            to[edges[i][0]].emplace_back(edges[i][1], succProb[i]);
-            to[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
         }
-        vector<double> Prob(n, 0);
-        queue<int> Q;
-        Q.push(start);
-        Prob[start] = 1;
-        while (!Q.empty()){
-            int node = Q.front();
-            Q.pop();
-            for (auto child : to[node]){
-                int nxt = child.first;
-                double p = child.second;
-                if (Prob[nxt] < Prob[node] * p) Prob[nxt] = Prob[node] * p, Q.push(nxt);
-            }
-        }
-        return Prob[end];
+        return ans[end];
     }
 };
