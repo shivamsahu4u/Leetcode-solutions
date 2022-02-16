2819f3de1c5a7b3c0a54a343df4cb0a2c86a9061
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-02-16 17:40:30 +0530

    Time: 4 ms (57.52%), Space: 7.6 MB (7.38%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -12,20 +12,49 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         
-        if(head == NULL){
+        if(head == NULL || head->next == NULL){
             return head;
         }
         
-        if(head->next == NULL){
-            return head;
+             ListNode* pre = head;
+       
+             ListNode*t1 = pre;
+             ListNode*t2 = pre->next;
+             ListNode*t3 = t2->next;
+             
+             head = t2;
+             t2->next = t1;
+             t1->next = t3;
+             pre = t3;
+            ListNode* n = t1;
+        while(pre!=NULL && pre->next!=NULL){
+            
+             ListNode*t1 = pre;
+             ListNode*t2 = pre->next;
+             ListNode*t3 = t2->next;
+             
+             t2->next = t1;
+             t1->next = t3;
+             pre = t3;
+             n->next = t2;
+             n = t1;
         }
         
-        ListNode*t1 = head;
-        ListNode*t2 = head->next;
-        ListNode*t3 = t2->next; // recursion part
+        return head;
+//         if(head == NULL){
+//             return head;
+//         }
         
-        t2->next = t1;
-        t1->next = swapPairs(t3);
-        return t2;
+//         if(head->next == NULL){
+//             return head;
+//         }
+        
+//         ListNode*t1 = head;
+//         ListNode*t2 = head->next;
+//         ListNode*t3 = t2->next; // recursion part
+        
+//         t2->next = t1;
+//         t1->next = swapPairs(t3);
+//         return t2;
     }
 };
