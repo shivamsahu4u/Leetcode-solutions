36a80dc3d5c82fbb0bdb98521842881da8e1d171
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-02-20 14:17:20 +0530

    Time: 23 ms (80.70%), Space: 11.4 MB (30.26%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,22 +1,59 @@
 class Solution {
 public:
+    
+    //This is O(N*N) Approach
+    
+    static bool comp(vector<int>&a , vector<int>&b){
+        
+        if(a[0] == b[0]){
+            return a[1] > b[1];
+        }else{
+            return a[0] > b[0];
+        }
+    }
+    
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int c = 0;
-        for(int i = 0 ; i < intervals.size(); i++){
+     
+        sort(intervals.begin() , intervals.end());
+        //sorting the intervals
+        
+        int x1 = intervals[0][0];
+        int x2 = intervals[0][1];
+        
+        int res = 1;
+        
+        //firstly including the point
+        
+        for(int i = 1 ; i < intervals.size() ; i++){
             
-            for(int j = 0 ; j < intervals.size() ; j++){
+            if(intervals[i][0] > x1 && intervals[i][1] > x2){
+                res++;
+            }
+            
+            if(intervals[i][1] > x2){
                 
-                if(i == j){
-                    continue;
-                }
-                
-                if(intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1]){
-                    c++;
-                    break;
-                }
+                x1 = intervals[i][0];
+                x2 = intervals[i][1];
             }
         }
-        
-        return intervals.size()-c;
+        return res;
     }
 };
+
+//    int c = 0;
+//         for(int i = 0 ; i < intervals.size(); i++){
+            
+//             for(int j = 0 ; j < intervals.size() ; j++){
+                
+//                 if(i == j){
+//                     continue;
+//                 }
+                
+//                 if(intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1]){
+//                     c++;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return intervals.size()-c;
