eb2c12aa708aa4bf198854aaea42558e248f7c93
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-02-22 01:30:10 +0530

    Time: 20 ms (90.60%), Space: 15 MB (46.71%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+ unordered_map<string , priority_queue<string , vector<string> , greater<string>>>hash ;
+   
+    void dfs(string u , unordered_map<string,bool>&visited , vector<string>&v){
+        
+       // visited[u] = true;
+       
+        
+        // if(hash[u].size() == 0){
+        //     return;
+        // }
+        
+       while(hash[u].size()!= 0){
+           string s = hash[u].top();
+           hash[u].pop();
+        dfs(s  , visited , v);
+             
+        }
+        
+         v.push_back(u);
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+     
+        // unordered_map<string , priority_queue<string , vector<string> , greater<string>>hash;
+        
+        for(auto it : tickets){
+            hash[it[0]].push(it[1]);
+           // hash[it[1]].insert(it[0]);
+        }
+        
+       unordered_map<string , bool>visited;
+        vector<string>v;
+       dfs("JFK"  , visited , v);
+        
+        reverse(v.begin() , v.end());
+        return v;
+    }
+};
