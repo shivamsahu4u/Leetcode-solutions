c733df23fd008622bb83714a304a878d70999ded
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-02-26 00:12:46 +0530

    Time: 3 ms (31.35%), Space: 6.2 MB (35.61%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,53 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+         int i = 0 , j = 0 , m  = version1.size() , n = version2.size();
+        
+        while(i < max(m,n) && j < max(n,m)){
+            
+            string s = "" , p = "";
+            
+            while(i < m && version1[i] == '0'){
+                i++;
+            }
+            while(i < m && version1[i] != '.'){
+                s = s + version1[i];
+                    i++;
+            }
+            
+            while(j < n && version2[j] == '0'){
+                j++;
+            }
+            while(j < n && version2[j] != '.'){
+                
+                p = p + version2[j];
+                j++;
+            }
+            
+            int x = 0; 
+               
+              if(s != "")  
+               x = stoi(s);
+            int y = 0; 
+                
+              if(p!= "")
+               y = stoi(p);
+            cout<<s<<" "<<p<<endl;
+            if(x == y){
+                i++;
+                j++;
+            }else if(x > y){
+                return 1;
+            }else{
+                return -1;
+            }
+             
+        }
+        
+    return 0; 
+    }
+};
