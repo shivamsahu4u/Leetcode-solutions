77dbf3aebfc0eba5e4dc82a4ed83af93c7db3657
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-03-02 16:29:23 +0530

    Time: 176 ms (80.87%), Space: 53.8 MB (61.66%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -1,63 +1,58 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        
-        //since we need to find root node
-        //we can make use of indeegre array
-        //since we know ,  answer will be center of gravity of graph
-        //so it may be 2 or 1 only
-        
-        vector<int>a;
+                vector<int>a;
         if(n == 0)return a;
         
         if(n == 1){
             a.push_back(0);
             return a;
         }
-        vector<int>arr[n];
         vector<int>indeegre(n , 0);
+        vector<int>arr[n];
         
-        for(int i = 0 ; i < edges.size() ; i++){
-            arr[edges[i][0]].push_back(edges[i][1]);
-            arr[edges[i][1]].push_back(edges[i][0]);    
-            indeegre[edges[i][0]]++;
-            indeegre[edges[i][1]]++;
+        for(auto path : edges){
+            int a = path[0];
+            int b = path[1];
+            arr[a].push_back(b);
+            arr[b].push_back(a);
+            indeegre[a]++;
+            indeegre[b]++;
         }
         
         queue<int>q;
         
-        for(int i = 0 ; i < indeegre.size() ; i++){
+        for(int i = 0 ; i < n ; i++){
+           
             if(indeegre[i] == 1){
                 q.push(i);
             }
         }
-        
         int count = n;
         while(count > 2){
             
+            int size = q.size();
+            count = count - q.size();
             
-             int coun = q.size();
-             count = count - q.size();
-            for(int i = 0; i < coun ; i++){
+            for(int i = 0 ; i < size ; i++){
                 
-                  int element = q.front();
-                  q.pop();
+                 int front = q.front();
+                 q.pop();
                 
-                 for(auto it : arr[element]){
-                     
-                       indeegre[it]--;
-                     
-                     if(indeegre[it] == 1){
-                         q.push(it);
-                     }
-                 }
+                for(auto it : arr[front]){
+                    
+                     indeegre[it]--;
+                   
+                    
+                    if(indeegre[it] == 1){
+                        
+                        q.push(it);
+                    }
+                }
             }
-            
-            
         }
-        
         vector<int>ans;
-        while(q.size()!=0){
+        while(q.size() != 0){
             ans.push_back(q.front());
             q.pop();
         }
@@ -65,3 +60,132 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        
+//         //since we need to find root node
+//         //we can make use of indeegre array
+//         //since we know ,  answer will be center of gravity of graph
+//         //so it may be 2 or 1 only
+        
+//         vector<int>a;
+//         if(n == 0)return a;
+        
+//         if(n == 1){
+//             a.push_back(0);
+//             return a;
+//         }
+//         vector<int>arr[n];
+//         vector<int>indeegre(n , 0);
+        
+//         for(int i = 0 ; i < edges.size() ; i++){
+//             arr[edges[i][0]].push_back(edges[i][1]);
+//             arr[edges[i][1]].push_back(edges[i][0]);    
+//             indeegre[edges[i][0]]++;
+//             indeegre[edges[i][1]]++;
+//         }
+        
+//         queue<int>q;
+        
+//         for(int i = 0 ; i < indeegre.size() ; i++){
+//             if(indeegre[i] == 1){
+//                 q.push(i);
+//             }
+//         }
+        
+//         int count = n;
+//         while(count > 2){
+            
+            
+//              int coun = q.size();
+//              count = count - q.size();
+//             for(int i = 0; i < coun ; i++){
+                
+//                   int element = q.front();
+//                   q.pop();
+                
+//                  for(auto it : arr[element]){
+                     
+//                        indeegre[it]--;
+                     
+//                      if(indeegre[it] == 1){
+//                          q.push(it);
+//                      }
+//                  }
+//             }
+            
+            
+//         }
+        
+//         vector<int>ans;
+//         while(q.size()!=0){
+//             ans.push_back(q.front());
+//             q.pop();
+//         }
+        
+//         return ans;
+//     }
+// };
