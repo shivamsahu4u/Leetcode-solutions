a2df3e3706073bbf05631fb1eb4b2c9ef7e9d7d1
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-03-02 13:37:09 +0530

    Time: 36 ms (6.62%), Space: 140.9 MB (5.07%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    //Solved by recursion
+    // Time Complexity 
     bool isSubsequence(string s, string t) {
         
          if(s == ""){
@@ -12,12 +15,19 @@ public:
         
         
         bool ans1 = false , ans2 = false;
-        if(s[0] != t[0])
+        if(s[0] != t[0]){
          ans1 = isSubsequence(s , t.substr(1));
+         if(ans1){
+             return true;
+         }
+        }
         
-        if(s[0] == t[0])
+        if(s[0] == t[0]){
          ans2 = isSubsequence(s.substr(1) , t.substr(1));
-        
+         if(ans2){
+             return true;
+         }
+        }
         return ans1||ans2;
     }
 };
