161de410a40ec857049ec0aab1a2efced2ca3a21
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-03-02 14:05:00 +0530

    Time: 6 ms (23.26%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,33 +1,56 @@
 class Solution {
 public:
     
-    //Solved by recursion
-    // Time Complexity 
     bool isSubsequence(string s, string t) {
-        
-         if(s == ""){
+              if(s == ""){
              return true;
          }
         
         if(t == ""){
             return false;
         }
+         int i = 0 , j = 0;
         
+          for(i = 0 ; i < t.size() ; i++){
+              
+              if(t[i] == s[j]){
+                  j++;
+                  
+                  if(j == s.size()){
+                      return true;
+                  }
+              }
+          }
         
-        bool ans1 = false , ans2 = false;
-        if(s[0] != t[0]){
-         ans1 = isSubsequence(s , t.substr(1));
-         if(ans1){
-             return true;
-         }
-        }
-        
-        if(s[0] == t[0]){
-         ans2 = isSubsequence(s.substr(1) , t.substr(1));
-         if(ans2){
-             return true;
-         }
-        }
-        return ans1||ans2;
+    return false;
     }
+    //Solved by recursion
+    // Recursion
+//     bool isSubsequence(string s, string t) {
+        
+//          if(s == ""){
+//              return true;
+//          }
+        
+//         if(t == ""){
+//             return false;
+//         }
+        
+        
+//         bool ans1 = false , ans2 = false;
+//         if(s[0] != t[0]){
+//          ans1 = isSubsequence(s , t.substr(1));
+//          if(ans1){
+//              return true;
+//          }
+//         }
+        
+//         if(s[0] == t[0]){
+//          ans2 = isSubsequence(s.substr(1) , t.substr(1));
+//          if(ans2){
+//              return true;
+//          }
+//         }
+//         return ans1||ans2;
+//     }
 };
