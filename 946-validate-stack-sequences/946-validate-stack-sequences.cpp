342c2402a39b02cbad2bcd821bf6a686715a822b
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-03-16 23:32:47 +0530

    Time: 11 ms (67.80%), Space: 15.5 MB (15.68%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -2,38 +2,58 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         
-         int n = pushed.size();
-         // vector<bool>visited(n+1 , false);
-        unordered_map<int , bool>visited; 
-        int k = 0;
-         stack<int>st;
-         st.push(pushed[0]);
-        
-         for(int i = 0 ; i < popped.size() ; i++){
-             
-             if(visited[popped[i]] == true){
-                 return false;
-             }
-             
-             if(st.size() > 0 && st.top() == popped[i]){
-                 int ele = st.top();
-                 st.pop();
-                 visited[ele] = true;
-             }else{
-                 k++;
-                while(k < n && pushed[k]!=popped[i] ){
-                  
-                    st.push(pushed[k]);
-                    k++;
-                }
-                 
-                if(k >= n){
-                    return false;
-                } 
-
-                visited[popped[i]] = true; 
-             }
-         }
-        return true;
+        stack<int>st;
+        int j = 0 ;
+        for(int i = 0 ; i < pushed.size() ; i++){
+            
+            st.push(pushed[i]);
+            
+            while(st.size() > 0 && st.top() == popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        return st.size() == 0;
     }
 };
+
+
+//TC - O(N)
+//SC - O(N)
+
+//  bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        
+//          int n = pushed.size();
+//          // vector<bool>visited(n+1 , false);
+//         unordered_map<int , bool>visited; 
+//         int k = 0;
+//          stack<int>st;
+//          st.push(pushed[0]);
+        
+//          for(int i = 0 ; i < popped.size() ; i++){
+             
+//              if(visited[popped[i]] == true){
+//                  return false;
+//              }
+             
+//              if(st.size() > 0 && st.top() == popped[i]){
+//                  int ele = st.top();
+//                  st.pop();
+//                  visited[ele] = true;
+//              }else{
+//                  k++;
+//                 while(k < n && pushed[k]!=popped[i] ){
+                  
+//                     st.push(pushed[k]);
+//                     k++;
+//                 }
+                 
+//                 if(k >= n){
+//                     return false;
+//                 } 
+
+//                 visited[popped[i]] = true; 
+//              }
+//          }
+//         return true;
+//     }
