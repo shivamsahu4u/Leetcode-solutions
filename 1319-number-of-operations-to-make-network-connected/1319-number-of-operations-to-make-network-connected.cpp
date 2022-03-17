5db70c8972df25a99d0be710de04abbc20bbedec
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-03-17 14:50:34 +0530

    Time: 169 ms (60.21%), Space: 54.8 MB (33.49%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,46 +1,128 @@
 class Solution {
 public:
     
-    void dfs(int u , vector<int>adj[] , vector<bool>&visited){
+    void dfs(int u , vector<int>arr[] , vector<bool>&visited){
         
         visited[u] = true;
         
-        for(auto it : adj[u]){
+        for(auto it : arr[u]){
             
             if(!visited[it]){
-                
-                dfs(it , adj , visited);
+                dfs(it , arr , visited);
             }
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
         
-        //its looking that we need to find the number of islands
+        vector<int>arr[n];
         
-        vector<int>adj[n];
-        vector<bool>visited(n , false);
-        
-        for(auto path : connections){
-            
-            adj[path[0]].push_back(path[1]);
-            adj[path[1]].push_back(path[0]);
+        for(auto it : connections){
+            arr[it[0]].push_back(it[1]);
+            arr[it[1]].push_back(it[0]);
         }
+        
+        vector<bool>visited(n , false);
         
         int count = 0;
         for(int i = 0 ; i < n ; i++){
             
             if(!visited[i]){
-                
                 count++;
-                
-                dfs(i , adj , visited);
+                dfs(i , arr , visited);
             }
         }
-        int x = connections.size();
         
-        if(n-1 > x){
+        int total = connections.size();
+        int needed = n-1;
+        
+        if(needed > total){
             return -1;
+        }else{
+            return count-1;
         }
-        return count-1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+    
+//     void dfs(int u , vector<int>adj[] , vector<bool>&visited){
+        
+//         visited[u] = true;
+        
+//         for(auto it : adj[u]){
+            
+//             if(!visited[it]){
+                
+//                 dfs(it , adj , visited);
+//             }
+//         }
+//     }
+//     int makeConnected(int n, vector<vector<int>>& connections) {
+        
+//         //its looking that we need to find the number of islands
+        
+//         vector<int>adj[n];
+//         vector<bool>visited(n , false);
+        
+//         for(auto path : connections){
+            
+//             adj[path[0]].push_back(path[1]);
+//             adj[path[1]].push_back(path[0]);
+//         }
+        
+//         int count = 0;
+//         for(int i = 0 ; i < n ; i++){
+            
+//             if(!visited[i]){
+                
+//                 count++;
+                
+//                 dfs(i , adj , visited);
+//             }
+//         }
+//         int x = connections.size();
+        
+//         if(n-1 > x){
+//             return -1;
+//         }
+//         return count-1;
+//     }
+// };
