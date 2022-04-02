92b1ec1f070ab236d67a710be9b71ba3fa0479ac
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-04-02 23:41:22 +0530

    Time: 109 ms (5.93%), Space: 23.9 MB (39.29%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    bool helper(string s , int i , int j){
+        
+         while(i < j){
+             
+             if(s[i]  != s[j]){
+                 return false;
+             }
+             i++;
+             j--;
+         }
+        
+        return true;
+    }
+    bool validPalindrome(string s) {
+        
+        //using two pointer
+         
+         int i = 0 ; int j = s.size() - 1;
+   
+         while(i < j){
+             
+             
+             
+             
+             
+             if(s[i] != s[j]){
+                 
+                 return helper(s , i+1 , j) || helper(s , i , j - 1);
+                 
+             }
+             
+             i++;
+             j--;
+             
+         
+        
+    }
+     return true;   
+    }
+};
