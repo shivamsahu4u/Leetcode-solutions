6d01537affdf9c4fa12ea9c07903887f70f37397
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-04-24 12:45:04 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -10,32 +10,72 @@ using namespace std;
 class Solution{
 public:
     // 5   0 1 2 3 4
-    int recursion(vector<vector<int>>&dp , int arr[] , int s , int e){
-        
     
-        if(s == e || s +1 == e){
-            return 0;
+    int dpp(int arr[] , int n){
+        
+        vector<vector<int>>dp(n , vector<int>(n , 0));
+        
+        for(int i = 1 ; i < n ; i++){
+            dp[i][i] = 0;
+            //single matrix have 0 multiplications
         }
         
-        if(dp[s][e] != -1){
-            return dp[s][e];
-        }
+        //since matrix will be filled crossly
         
-        int ans = INT_MAX;
-        for(int i = s+1 ; i < e ; i++){
-            int left = recursion(dp,arr , s , i);
-            int right = recursion(dp,arr , i , e);
-            int cost = left+right+arr[s]*arr[i]*arr[e];
-            ans = min(ans , cost);
+        for(int l = 2 ; l < n; l++){
+            
+            for(int i = 1 ; i < n-l+1 ; i++){
+                
+                int j = i + l - 1;
+                dp[i][j] = INT_MAX;
+                for(int k = i ; k < j ; k++){
+                    
+                    dp[i][j] = min(dp[i][j] , dp[i][k] + dp[k+1][j] + arr[i-1]*arr[k]*arr[j]);
+                }
+            }
+            
         }
-        return dp[s][e] = ans;
+        return dp[1][n-1];
     }
     int matrixMultiplication(int N, int arr[])
     {
         vector<vector<int>>dp(N+1 , vector<int>(N+1 , -1));
-       return recursion(dp , arr , 0 , N-1);
+       return dpp( arr , N);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int recmemo(vector<vector<int>>&dp , int arr[] , int s , int e){
+        
+    
+//         if(s == e || s +1 == e){
+//             return 0;
+//         }
+        
+//         if(dp[s][e] != -1){
+//             return dp[s][e];
+//         }
+        
+//         int ans = INT_MAX;
+//         for(int i = s+1 ; i < e ; i++){
+//             int left = recursion(dp,arr , s , i);
+//             int right = recursion(dp,arr , i , e);
+//             int cost = left+right+arr[s]*arr[i]*arr[e];
+//             ans = min(ans , cost);
+//         }
+//         return dp[s][e] = ans;
+//     }
 
 // { Driver Code Starts.
 
