19f7e0e81849a6d3b7363876212f1462e018689a
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-04-24 16:20:11 +0530

    Time: 241 ms (11.81%), Space: 49.1 MB (13.53%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -23,9 +23,37 @@ public:
         
         return dp[u][count] = min(ans2 , ans1);
     }
+    int dpp(vector<int>&coins , int amount){
+        vector<vector<int>>dp(coins.size()+1, vector<int>(amount+1 , -1));
+        
+        for(int i = 1 ; i <= amount ; i++)
+            dp[0][i] = INT_MAX-1;
+        
+        for(int i = 0 ; i <= coins.size() ; i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int i= 1 ; i <= coins.size() ; i++){
+            
+            for(int j = 1 ; j <= amount ; j++){
+                
+                int ans1 = INT_MAX , ans2 = INT_MAX;
+                if(coins[i-1] <= j){
+                    ans1 = 1 + dp[i][j - coins[i-1]];
+                }
+                
+                ans2 = dp[i-1][j];
+                
+                dp[i][j] = min(ans1 , ans2);
+            }
+        }
+        return dp[coins.size()][amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(), vector<int>(amount+1 , -1));
-        int p = helper(dp, coins , 0 , coins.size() , amount);
+        // vector<vector<int>>dp(coins.size(), vector<int>(amount+1 , -1));
+        // int p = helper(dp, coins , 0 , coins.size() , amount);
+        // return p== INT_MAX-1?-1:p;
+        int p =  dpp(coins , amount);
         return p== INT_MAX-1?-1:p;
     }
 };
