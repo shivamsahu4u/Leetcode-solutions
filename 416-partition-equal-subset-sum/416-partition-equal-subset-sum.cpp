3b681dc2f8820ecad632381d6b114a331bdfa58a
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-04-28 17:02:22 +0530

    Time: 155 ms (71.64%), Space: 9.1 MB (93.53%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,49 +2,33 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         
-        // Recursive : Using two variables
+           int count = 0;
+     
+         for(int i = 0; i  < nums.size() ; i++){
+             count  = count  + nums[i];
+         }
         
-        // Recursive + Memoization : Using Intution 
-        // Doing half and find sum to half
+        int half= count/2;
         
-        // Dynamic Programming
-        int cost = 0;
-        for(int i = 0 ; i < nums.size() ; i++){
-            cost = cost + nums[i];
-        }
-        // means it is odd and cant be divided into two parts
-        if(cost % 2 != 0){
+        if(count % 2 != 0){
             return false;
         }
         
-        int sum = cost/2;
+        vector<bool>dp(half + 1 , false);
         
-        bool dp[nums.size()+1][sum+1];
+        dp[0] = true;
         
-        for(int i = 1 ; i < sum+1 ; i++){
-            dp[0][i] = false;
-        }
         
-        for(int i = 0 ; i < nums.size()+1 ; i++){
-            dp[i][0] = true;
-        }
-        
-        for(int i = 1 ; i < nums.size()+1 ; i++){
+        for(int i = 0 ; i < nums.size() ; i++){
             
-            for(int j = 1 ; j < sum + 1 ; j++){
+            for(int j = half ; j >= nums[i] ; j--){
                 
-                //either I will take it or not
-                // I will take element only when its nums[i-1] <= j
-                bool ans1 = false , ans2 = false;
-                if(nums[i-1] <= j){
-                    // Yes i included this element
-                    ans1 = dp[i-1][j - nums[i-1]];
+                if(dp[j-nums[i]]){
+                    dp[j]=true;
                 }
-                    ans2 = dp[i-1][j];
-                
-                dp[i][j] = ans1 || ans2;
             }
         }
-        return dp[nums.size()][sum];
+        
+        return dp[half];
     }
 };
