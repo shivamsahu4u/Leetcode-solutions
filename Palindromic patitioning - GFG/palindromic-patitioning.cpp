4d73e07158506bd9b31bedb1e68a40841b3df17f
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-04-28 15:08:31 +0530

    Added solution - LeetHub

--- Palindromic patitioning - GFG/palindromic-patitioning.cpp
+++ Palindromic patitioning - GFG/palindromic-patitioning.cpp
@@ -11,46 +11,53 @@ class Solution{
 public:
     int palindromicPartition(string str)
     {
-        // code here
-        // 5 aaabb
         int n = str.size();
-        int c[n][n];
+        int c[n];
         int p[n][n];
+        //aaabb
+        //we can first find out the plimdrome of all substrings
+        // 2 3 4 5
+            for (int i = 0; i < n; i++) {
+        p[i][i] = true;
+    }
+    
+        for(int l = 2 ; l <= n ; l++){
+         
+         //                  5 - 5 + 1
+          for(int i = 0 ; i < n-l+1 ; i++){
+              
+              //  0 + 5 - 1 = 4
+              int j = i + l - 1;
+              
+              if(l == 2){
+                  p[i][j] = str[i]==str[j];
+              }else{
+                  
+                  p[i][j] = (str[i]==str[j]) && (p[i+1][j-1]);
+              }
+          }
+        }
         
-       for(int i= 0 ; i < n ; i++){
-           c[i][i] = 0;
-           p[i][i] = true;
-       }    
-       // 2 3 4     // 4
-       for(int l = 2 ; l <= n ; l++){
-           
-           // 0 1 2 3     // 0 1
-           for(int i = 0 ; i < n-l+1 ; i++){
-               
-               // 1 2 3 4        // 
-               int j = i + l - 1;
-               
-               if(l == 2){
-                   // it means substring is of length 2
-                   p[i][j] = str[i] == str[j];
-               }else{
-                   
-                   p[i][j] = ((str[i] == str[j]) && p[i+1][j-1]);
-               }
-               
-               if(p[i][j] == true){
-                   c[i][j] = 0;
-               }else{
-               c[i][j] = INT_MAX;
-               for(int k = i ; k < j ; k++){
-                   
-                    int ans = c[i][k] + c[k+1][j] + 1;
-                    c[i][j] = min(c[i][j] , ans);
-               }
-               }
-           }
-       }
-       return c[0][n-1];
+        // now we have answers that 
+        // from i to j , weahter it is palimdrome or not
+        
+        for(int i = 0 ; i < n ; i++){
+            
+              if(p[0][i] == true){
+                  c[i] = 0;
+              }else{
+                  
+                  c[i] = INT_MAX;
+                  
+                  for(int j = 0 ; j < i ; j++){
+                      
+                       if(p[j+1][i] == true && 1 + c[j] < c[i]){
+                           c[i] = c[j] + 1;
+                       }
+                  }
+              }
+        }
+        return c[n-1];
     }
 };
 
