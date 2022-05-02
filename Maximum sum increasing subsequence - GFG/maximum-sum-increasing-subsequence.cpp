bd70252a7775952000ec3f6275f07aae9e5ecedc
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-05-02 12:27:07 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -5,7 +5,8 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 		
-
+// Time Complexity - O(n2)
+// Space Complexity - O(n)
 	public:
     int dp(int arr[] , int n){
         
@@ -27,6 +28,9 @@ class Solution{
         }
         return ans;
     }
+    
+    // TC - O(2^n)  SC O(N)
+    // MEMO TC- O(N2) SC - O(N2)
 	int recursion(int arr[] , int s , int e , int pre , vector<vector<int>>&dp){
 	    
 	    if(s == e){
@@ -39,7 +43,7 @@ class Solution{
 	    }
 	    
 	    int include = 0; int notinclude= 0;
-	    if(arr[pre] < arr[s]){
+	    if(pre == -1 || arr[pre] < arr[s]){
 	        
 	        include = arr[s] + recursion(arr , s + 1 , e , s , dp);
 	    }
@@ -51,10 +55,10 @@ class Solution{
 	int maxSumIS(int arr[], int n)  
 	{  
 	    // index + previous index
-	   // vector<vector<int>>dp(n+1 ,vector<int>(n+1 , -1));
-	   // return recursion(arr , 0 , n , -1 , dp);
+	    vector<vector<int>>dp(n+1 ,vector<int>(n+1 , -1));
+	    return recursion(arr , 0 , n , -1 , dp);
 	   
-	   return dp(arr,  n);
+	   //return dp(arr,  n);
 	}  
 };
 
