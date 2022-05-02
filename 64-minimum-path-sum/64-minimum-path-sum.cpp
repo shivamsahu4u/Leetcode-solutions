c332cdbef107920a08811575ac5f5775b094137d
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-05-02 13:57:20 +0530

    Time: 16 ms (30.28%), Space: 10.2 MB (27.89%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -35,24 +35,24 @@ public:
         int c = 0;
         for(int i = 0 ; i < m ; i++){
             c += grid[i][0];
-            dp[i][0] = c;
+            grid[i][0] = c;
         }
          c = 0;
         for(int i = 0 ; i < n ; i++){
             c += grid[0][i];
-            dp[0][i] = c;
+            grid[0][i] = c;
         }
         
         for(int i = 1 ; i < m ; i++){
             
             for(int j = 1 ; j < n ; j++){
                 
-                dp[i][j] =  grid[i][j] + dp[i-1][j];
-                dp[i][j] = min(dp[i][j] , grid[i][j] + dp[i][j-1]);
+                int c =  grid[i][j] + grid[i-1][j];
+                grid[i][j] = min(c , grid[i][j] + grid[i][j-1]);
             }
         }
         
-        return dp[m-1][n-1];
+        return grid[m-1][n-1];
     }
     int minPathSum(vector<vector<int>>& grid) {
     return dpp(grid);
