95d8eec3361a7ca080322946f84040abae69e8e0
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-05-29 17:39:20 +0530

    Time: 19 ms (56.81%), Space: 5.9 MB (33.37%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,20 +2,40 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-         int y = abs(x);
-        
-         string st = to_string(x);
-        
-        int i = 0 , j = st.size()-1;
-        
-        while(i < j){
-            
-            if(st[i] != st[j]){
-                return false;
-            }
-            i++;
-            j--;
+     long long y = 0 , z = x;
+    
+        if(x < 0){
+            return false;
         }
-        return true;
+     while(x > 0){
+         
+          long long p = x % 10;
+         
+          y = y*10 + p;
+         
+          x = x / 10;
+     }
+        
+      return y == z;  
+        
+//         // Time Complexity is O(n)
+//          int y = abs(x);
+        
+//          // this step takes O(n) time 
+//          string st = to_string(x);
+        
+//         int i = 0 , j = st.size()-1;
+        
+//         // O(n)
+//         while(i < j){
+            
+            
+//             if(st[i] != st[j]){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
     }
 };
