d18f71e4ba841165ec32a356ef61f2580d9a8604
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-06-02 00:22:33 +0530

    Time: 15 ms (34.88%), Space: 9.5 MB (87.29%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,22 +2,53 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        // hashmap
-        unordered_map<int,int>hash;
-        for(int i = 0 ; i < nums.size() ; i++){
-            hash[nums[i]]++;
+        // o(nlogn) time complexity
+        sort(nums.begin() , nums.end());
+        int ans;
+        
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        if(nums.size() > 1){
+           if(nums[1] != nums[0]){
+               ans = nums[0];
+           }else{
+               ans = -1;
+           }    
+        }
+       
+        for(int i = 1 ; i < nums.size() ; i++){
+            
+if(nums[i-1] != nums[i] && (i+1 < nums.size() && nums[i]!=nums[i+1])){
+                 ans = nums[i];
+             }
         }
         
-        int m = INT_MAX;
-        int index;
-        for(auto it = hash.begin() ; it!= hash.end() ; it++){
-           if(m > it->second){
-               m = it->second;
-               index = it->first;
-           }
+        if(ans == -1){
+            return nums[nums.size() - 1];
+        }else{
+            return ans;
         }
-        return index;
         
-        // frequency array 
+        
+        // O(N) time complexity
+        // O(N) space complexity
+        // hashmap  // frequency array 
+//         unordered_map<int,int>hash;
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             hash[nums[i]]++;
+//         }
+        
+//         int m = INT_MAX;
+//         int index;
+//         for(auto it = hash.begin() ; it!= hash.end() ; it++){
+//            if(m > it->second){
+//                m = it->second;
+//                index = it->first;
+//            }
+//         }
+//         return index;
+        
+       
     }
 };
