797aa562c48a2b4a80f2388c144ee5098f670370
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-07-13 14:46:09 +0530

    Time: 16 ms (82.83%), Space: 13.3 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -30,42 +30,33 @@ public:
              }
          }
          
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                
-                 
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
                  if(matrix[i][j] == 0){
-                     
                       matrix[i][0] = 0;
                       matrix[0][j] = 0;
                  }
             }
         }
         
-        for(int i = 1 ; i < m ; i++){
-            
+        for(int i = 1 ; i < m ; i++){  
             for(int j = 1  ; j < n ; j++){
-                
           
                  if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                     
                      matrix[i][j] = 0;
                  }
             }
         }
          
+         if(rows == true){
          for(int i = 0 ; i < m ; i++){
-              if(rows == true){
                   matrix[i][0] = 0;
-              }
-         }
+         }}
          
+         if(column == true){
          for(int i = 0 ; i < n ; i++){
-             
-             if(column == true){
-                 matrix[0][i] = 0;
-             }
-         }
+                 matrix[0][i] = 0; 
+         }}
         
      } 
          
