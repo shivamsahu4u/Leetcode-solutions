971049ac9b83d2d38241e1a9eb2df09fd309a5f9
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-07-17 00:44:53 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    
+    // Brute Force 
+    // TC - O(numRows * numRows)
+    // SC - O(numRows * numRows)
+    
+    // Better Solution
+    // TC - O(numRows * numRows)
+    // SC - O(1)
     vector<vector<int>> generate(int numRows) {
                     // 4 4
         //int matrix[30][30] = {0};
@@ -16,13 +24,14 @@ public:
         for(int i = 1 ; i < numRows ; i++){
                    // 0 1  0 1 2  0 1 2 3
             ans[i].resize(i+1);
-            for(int j = 0 ; j < i+1 ; j++){
+            ans[i][0] = 1 ; ans[i][i] = 1;
+            for(int j = 1 ; j < i ; j++){
                 
-                 if(j == 0)    // 0 0   0 0 + 0 1
-                 ans[i][j] = ans[i-1][j];
-                 else if(j == i)
-                 ans[i][j] = ans[i-1][j-1];
-                 else
+                 // if(j == 0)    // 0 0   0 0 + 0 1
+                 // ans[i][j] = ans[i-1][j];
+                 // else if(j == i)
+                 // ans[i][j] = ans[i-1][j-1];
+                 // else
                  ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
             }
             
