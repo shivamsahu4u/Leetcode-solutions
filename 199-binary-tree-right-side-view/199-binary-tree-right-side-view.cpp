23e67a840b1a956f3ef2d4b1dfc89fc3ffaed0cc
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-07-24 00:06:09 +0530

    Time: 0 ms (100.00%), Space: 12 MB (67.08%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void helper(TreeNode*root , int level , vector<int>&ans){
+        
+        if(root == NULL){
+            return;
+        }
+        
+        if(ans.size() < level){
+            ans.push_back(root->val);
+        }
+        
+        helper(root->right , level + 1 , ans);
+        helper(root->left , level + 1 ,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+     
+        
+         vector<int>ans;
+        
+         helper(root , 1 , ans);
+        
+        return ans;
+         //first method is through level order traversal
+         // Time Complexity is O(n)
+        //Space Complexity is O(h)(we are using queue here)
+        
+        /*vector<int>ans;
+        if(root == NULL){
+            return ans;
+        }
+        
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+        vector<int>v;
+        while(q.size() != 0){
+            
+            TreeNode*front = q.front();
+            q.pop();
+            
+            if(front == NULL){
+                ans.push_back(v[v.size() -1]);
+                v.clear();
+                
+                if(q.size() == 0){
+                    return ans;
+                }
+                
+                q.push(NULL);
+            }else{
+                
+                v.push_back(front->val);
+                
+                if(front->left!=NULL){
+                    q.push(front->left);
+                }
+                
+                if(front->right != NULL){
+                    q.push(front->right);
+                }
+            }
+        }
+        return ans;*/
+    }
+};
