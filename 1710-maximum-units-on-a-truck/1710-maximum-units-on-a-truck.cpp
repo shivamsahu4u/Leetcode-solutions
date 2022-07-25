47e90b92937aa3d8f3576321072310ea38ab3c94
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-07-26 05:05:30 +0530

    Time: 345 ms (10.55%), Space: 49.4 MB (18.58%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+     static bool comp(vector<int> a , vector<int>b){
+         
+         if(a[1] > b[1]){
+             return true;
+         }
+         return false;
+     }
+    int maximumUnits(vector<vector<int>>& b, int t) {
+
+         sort(b.begin() , b.end() , comp);
+        // for(int i = 0 ; i < b.size() ; i++){
+        //     cout<<" "<<b[i][1]<<" ";
+        // }
+        int n = b.size();
+        int j = t;
+        
+        int ans = 0;
+        for(int i = 0 ; i < n && j > 0 ; i++){
+            
+            int s = b[i][0];
+            int p = b[i][1];
+            
+            if(s >= j){
+            
+                ans = ans + j*p;
+                j = 0;
+            }else{
+                
+                ans = ans + (s)*p;
+             cout<<" j s"<<j<<" "<<s<<" ";
+                j = j - s;
+                
+            }
+            // cout<<j<<" ";
+        }
+        
+        return ans;
+    }
+};
