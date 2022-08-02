2e06c3e2120bedb5c23240410315efd7d8c7a8e9
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-02 18:48:39 +0530

    Time: 10 ms (58.05%), Space: 8.9 MB (32.98%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,27 +2,54 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
      
-        unordered_map<int,int>hash;
-         for(int i = 0 ; i  < nums2.size() ; i++){
-             hash[nums2[i]] = i;
-         }
+          stack<int>s;
+        unordered_map<int , int>hash;
         
-        vector<int>v;
-        for(int i = 0 ; i < nums1.size(); i++){
+        for(int i : nums2){
             
-            int k = hash[nums1[i]];
-            int index = -1;
-            for(int j = k+1 ; j < nums2.size() ; j++){
+            while(s.size() && s.top() < i){
                 
-                if(nums1[i] < nums2[j]){
-                    index = nums2[j];
-                    break;
-                }
+                 hash[s.top()] = i;
+                 s.pop();
             }
             
-            v.push_back(index);
+            s.push(i);
         }
         
-        return v;
+        vector<int>ans;
+        
+        for(int j : nums1){
+            
+             ans.push_back(hash.count(j) ? hash[j] : -1);
+        }
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+     
+//         unordered_map<int,int>hash;
+//          for(int i = 0 ; i  < nums2.size() ; i++){
+//              hash[nums2[i]] = i;
+//          }
+        
+//         vector<int>v;
+//         for(int i = 0 ; i < nums1.size(); i++){
+            
+//             int k = hash[nums1[i]];
+//             int index = -1;
+//             for(int j = k+1 ; j < nums2.size() ; j++){
+                
+//                 if(nums1[i] < nums2[j]){
+//                     index = nums2[j];
+//                     break;
+//                 }
+//             }
+            
+//             v.push_back(index);
+//         }
+        
+//         return v;
+//     }
+// };
