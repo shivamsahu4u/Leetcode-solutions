f7f572b942d9dc1d56baca8b887f86b902a95435
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-10 15:38:22 +0530

    Time: 225 ms (66.67%), Space: 87.6 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -11,23 +11,52 @@ public:
           return (ind >=2 && nums[ind]-1 == nums[ind-1] && nums[ind-1]-1== nums[ind-2]);   
     }
     
-    bool check(int n , vector<int>&nums , vector<int>&hash){
+//     bool check(int n , vector<int>&nums , vector<int>&hash){
         
-        if(n+1 == 0){
+//         if(n+1 == 0){
+//             return true;
+//         }
+        
+//         if(hash[n] != -1){return hash[n];}
+//         int ok = false;
+        
+//         if(case1(n , nums))ok=ok|check(n-2 , nums,hash);
+//         if(case2(n , nums))ok=ok|check(n-3 , nums,hash);
+//         if(case3(n , nums))ok=ok|check(n-3 , nums,hash);
+        
+//         return hash[n]=ok;
+//     }
+    bool dpp(vector<int>&nums , int u , int v){
+        vector<int>dp(nums.size()+1 , -1);
+        
+        if(nums.size() == 0)
             return true;
+        if(nums.size() == 1){
+            return false;
+        }
+        dp[0] = true;
+        dp[1] = false;
+        if(case1(0 , nums)||case2(0,nums)||case3(0,nums))dp[1] = true;
+        dp[2] = false;
+        if(case1(1 , nums)||case2(1,nums)||case3(1,nums))dp[2] = true;
+        
+        for(int i = 3 ; i <=v ; i++){
+            
+             bool ok = false;
+            
+            if(case1(i-1 , nums))ok = ok | dp[i-2];
+            if(case2(i-1 , nums))ok = ok | dp[i-3];
+            if(case3(i-1 , nums))ok = ok | dp[i-3];
+            
+            dp[i] = ok;
         }
         
-        if(hash[n] != -1){return hash[n];}
-        int ok = false;
-        
-        if(case1(n , nums))ok=ok|check(n-2 , nums,hash);
-        if(case2(n , nums))ok=ok|check(n-3 , nums,hash);
-        if(case3(n , nums))ok=ok|check(n-3 , nums,hash);
-        
-        return hash[n]=ok;
+   
+        return dp[v];
     }
     bool validPartition(vector<int>& nums) {
-        vector<int>hash(nums.size() , -1);
-         return check(nums.size() - 1 , nums , hash);
+        // vector<int>hash(nums.size() , -1);
+         // return check(nums.size() - 1 , nums , hash);
+        return dpp(nums , 0 , nums.size() );
     }
 };
