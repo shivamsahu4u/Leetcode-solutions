efc39b7f3b56032955e6ff11dfe957b1cb084a57
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-13 19:53:04 +0530

    Time: 3 ms (64.07%), Space: 8.3 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -8,22 +8,41 @@ public:
         
         // Method 2 ( Using Frequency Array)
         // TC - O(N)
-        // SC - O(N)
+        // SC - O(1)
         
-        int arr[3] = {0};
+//         int arr[3] = {0}; // Constant Space
         
-        for(int i = 0 ; i < nums.size() ; i++){
-            arr[nums[i]]++;
-        }
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             arr[nums[i]]++;
+//         }
         
-        int j = 0;
-        for(int i = 0 ; i < nums.size() ; i++){
+//         int j = 0;
+//         for(int i = 0 ; i < nums.size() ; i++){
             
-            while(arr[j] == 0){
+//             while(arr[j] == 0){
+//                 j++;
+//             }
+//             nums[i] = j;
+//             arr[j]--;
+//         }
+        int i = 0 , k = nums.size()-1;
+        for(int j = 0 ; j <= k;){
+            
+            if(nums[j] == 0){
+                swap(nums[i] , nums[j]);
+                i++;
                 j++;
             }
-            nums[i] = j;
-            arr[j]--;
+            
+          else if(nums[j] == 1){
+                j++;
+            }
+            else if(nums[j] == 2){
+                swap(nums[j] , nums[k]);
+                k--;
+            }
+               
+           
         }
         
     }
