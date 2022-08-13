3824c303123e4726f832626a3aa5f02f43925113
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-14 02:22:28 +0530

    Time: 186 ms (40.85%), Space: 67.7 MB (90.81%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
+// TC - O(N)
+// SC - O(1)     
     int maxSubArray(vector<int>& arr) {
-        //   int n = arr.size();
-        //   int dp[n];
-        //  // dp[i] means maximum subarray till arr[i]
-        //   dp[0] = arr[0];
-        // // int sum = arr[0];
-        // int ans = arr[0];
-        // for(int i = 1 ; i < n ; i++){
-        //      // sum = sum + arr[i];
-        //      dp[i] = arr[i] + (dp[i-1] > 0 ? dp[i-1]: 0) ;
-        //     ans = max(ans , dp[i]);
-        // }
-        // return ans;
         
         int n = arr.size();
-        int dp[n];
         int ans = INT_MIN;
         int  m = 0;
         for(int i = 0 ; i < n ; i++){
-            
             m  = m + arr[i];
             ans = max(m , ans);
             if(m < 0){
@@ -31,38 +19,21 @@ public:
     }
 };
 
-//         // TC - O(N2)
-//         // SC - O(1)
-        
-//          int n = arr.size();
-//         int ans = INT_MIN;
-//         for(int i = 0 ; i < n ; i++){
-//              int sum = 0;
-               
-//             for(int j = i ; j < n ; j++){
-                                    
-//                 sum = sum + arr[j];
-//                 ans = max(ans , sum);
-//             }
-//         }
-//         return ans;
+// TC - O(N)
+// SC - O(N)
+//     int maxSubArray(vector<int>& arr) {
+//           int n = arr.size();
+//           int dp[n];
+//          // dp[i] means maximum subarray till arr[i]
+//           dp[0] = arr[0];
        
-//     }    
-    
-
-//  int n = arr.size();
-//         int ans = INT_MIN;
-//         for(int i = 0 ; i < n ; i++){
+//         int ans = arr[0];
+//         for(int i = 1 ; i < n ; i++){
             
-//             for(int j = 0 ; j < n ; j++){
-                
-//                 int sum = 0;
-//                 for(int k = i ; k <= j ; k++){
-                    
-//                     sum = sum + arr[k];
-//                 }
-                
-//                 ans = max(ans , sum);
-//             }
+//             dp[i] = arr[i] + (dp[i-1] > 0 ? dp[i-1]: 0) ;
+//             ans = max(ans , dp[i]);
 //         }
 //         return ans;
+        
+      
+//     }
