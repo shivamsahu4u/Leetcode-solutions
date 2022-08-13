f2272ac5e63bcf1244e33a5b6b35ef5d9d26a431
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-14 02:12:18 +0530

    Time: 130 ms (83.79%), Space: 67.8 MB (10.92%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
-          int n = arr.size();
-          int dp[n];
-         // dp[i] means maximum subarray till arr[i]
-          dp[0] = arr[0];
-        // int sum = arr[0];
-        int ans = arr[0];
-        for(int i = 1 ; i < n ; i++){
-             // sum = sum + arr[i];
-             dp[i] = arr[i] + (dp[i-1] > 0 ? dp[i-1]: 0) ;
-            ans = max(ans , dp[i]);
+        //   int n = arr.size();
+        //   int dp[n];
+        //  // dp[i] means maximum subarray till arr[i]
+        //   dp[0] = arr[0];
+        // // int sum = arr[0];
+        // int ans = arr[0];
+        // for(int i = 1 ; i < n ; i++){
+        //      // sum = sum + arr[i];
+        //      dp[i] = arr[i] + (dp[i-1] > 0 ? dp[i-1]: 0) ;
+        //     ans = max(ans , dp[i]);
+        // }
+        // return ans;
+        
+        int n = arr.size();
+        int dp[n];
+        int ans = INT_MIN;
+        int  m = 0;
+        for(int i = 0 ; i < n ; i++){
+            
+            m  = m + arr[i];
+            ans = max(m , ans);
+            if(m < 0){
+                m = 0;
+            }
+            
         }
         return ans;
     }
