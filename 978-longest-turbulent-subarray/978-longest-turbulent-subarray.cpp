5fc6d7b04630bf34b0661be3fbb11269ad320bf9
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-14 17:15:09 +0530

    Time: 211 ms (7.25%), Space: 43.6 MB (14.08%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -11,17 +11,24 @@ public:
         // longest turbuleny subarray ending at i such that arr[i] < arr[i-1]
         
         int ans = 1;
-        for(int i = 1 ; i < n ; i++){
-            
+        int pt =1, p = 1 , rt=1 , r = 1;
+        for(int i = 1 ; i < n ; i++){                 //1 1 1 1 1
+                                                     // 1 1 1 1 1
             if(arr[i] < arr[i-1]){
-                dp1[i] = 1 + dp2[i-1]; 
+                // dp1[i] = 1 + dp2[i-1];
+                pt = 1 + r;
             }
             
             if(arr[i] > arr[i-1]){
-                dp2[i] = 1 + dp1[i-1];
+                // dp2[i] = 1 + dp1[i-1];
+                rt = 1 + p;
             }
             
-            ans = max({ans , dp1[i] , dp2[i]});
+            ans = max({ans ,rt, pt});
+            // ans = max({ans , dp1[i] , dp2[i]});
+
+            p = pt; r = rt;
+                                     pt = 1 ; rt = 1;
         }
         return ans;
         
