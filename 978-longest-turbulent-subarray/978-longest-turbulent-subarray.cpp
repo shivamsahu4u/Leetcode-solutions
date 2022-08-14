fec55326fe53c76b1d71dfbf07bd8c24fd012586
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-14 17:15:29 +0530

    Time: 161 ms (19.95%), Space: 40.3 MB (89.29%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -4,10 +4,10 @@ public:
         
         int n = arr.size();
  
-        vector<int>dp1(n , 1);
+        // vector<int>dp1(n , 1);
         // longest turbulent subarray ending at i such that arr[i] > arr[i-1]
         
-        vector<int>dp2(n , 1);
+        // vector<int>dp2(n , 1);
         // longest turbuleny subarray ending at i such that arr[i] < arr[i-1]
         
         int ans = 1;
