e5b5df4a358d1a86bc361a16e9a808b72e8baedf
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-17 01:16:27 +0530

    Time: 8 ms (78.34%), Space: 14 MB (9.28%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        
-        // using kadane algorithm
-        
-        int m = 1;
-        int i = 0;
-        int n = nums.size();
-        int ans = INT_MIN;
-        while(i < n){
-            
-            m = m * nums[i];
-            ans = max(ans , m);
-            
-            if(m ==0){
-                m = 1;
-            }
-            i++;
-        }
-        
-        i = nums.size()-1;
-        m = 1;
-        while(i >= 0){
-            
-            m = m * nums[i];
-            ans = max(ans , m);
-            
-            if(m == 0){
-                m = 1;
-            }
-            i--;
-        }
-        return ans;
+    int maxProduct(vector<int>A) {
+    // store the result that is the max we have found so far
+    int r = A[0];
+      int n = A.size();
+    // imax/imin stores the max/min product of
+    // subarray that ends with the current number A[i]
+    for (int i = 1, imax = r, imin = r; i < n; i++) {
+        // multiplied by a negative makes big number smaller, small number bigger
+        // so we redefine the extremums by swapping them
+        if (A[i] < 0)
+            swap(imax, imin);
+
+        // max/min product for the current number is either the current number itself
+        // or the max/min by the previous number times the current one
+        imax = max(A[i], imax * A[i]);
+        imin = min(A[i], imin * A[i]);
+
+        // the newly computed max value is a candidate for our global result
+        r = max(r, imax);
     }
+    return r;
+}
 };
