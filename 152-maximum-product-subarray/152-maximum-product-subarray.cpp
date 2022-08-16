dd77ce351815f419bb8d00ca2f1f0954747f0fee
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-17 01:41:42 +0530

    Time: 8 ms (78.34%), Space: 14.5 MB (6.01%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
-    int maxProduct(vector<int>A) {
-    // store the result that is the max we have found so far
-    int r = A[0];
-      int n = A.size();
-    // imax/imin stores the max/min product of
-    // subarray that ends with the current number A[i]
+   int f(vector<int>A){
+         int r = A[0]; int n = A.size();
+ 
     for (int i = 1, imax = r, imin = r; i < n; i++) {
-        // multiplied by a negative makes big number smaller, small number bigger
-        // so we redefine the extremums by swapping them
+      
         if (A[i] < 0)
             swap(imax, imin);
 
-        // max/min product for the current number is either the current number itself
-        // or the max/min by the previous number times the current one
+      
         imax = max(A[i], imax * A[i]);
         imin = min(A[i], imin * A[i]);
 
-        // the newly computed max value is a candidate for our global result
+       
         r = max(r, imax);
     }
     return r;
-}
+
+   }
+    int maxProduct(vector<int>A) {
+      // lets try through dp
+        int n = A.size();
+        vector<int>mx(n , 0);
+        vector<int>mn(n , 0);
+        mx[0] = A[0];
+        mn[0] = A[0];
+        int ans = A[0];
+        for(int i = 1 ; i < n ; i++){
+            
+            if(A[i] > 0){
+             mx[i] = max(A[i] , A[i]*mx[i-1]);
+             mn[i] = min(A[i] , A[i]*mn[i-1]);   
+            }
+            else{
+                mx[i] = max(A[i], A[i] * mn[i-1]);
+                mn[i] = min(A[i] , A[i] * mx[i-1]);
+            }
+            ans = max(ans , mx[i]);
+        }
+        return ans;
+    }
 };
