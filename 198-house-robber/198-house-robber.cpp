9e8ec92c81f81e13d0b76d457a2fe5c17a692b3e
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-08-17 01:54:41 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -13,10 +13,25 @@ public:
         
         return dp[u] = max(rob(nums , u+2 , v , dp)+nums[u] , rob(nums , u+1 , v , dp));
     }
+    
+    int rob(vector<int>&nums , int n){
+        vector<int>dp(n+1, 0);
+        
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2 ; i <= n ; i++){
+            
+             dp[i] = max(nums[i-1] + dp[i-2] , dp[i-1]);
+        }
+        
+        return dp[n];
+        
+    }
     int rob(vector<int>& nums) {
          
          int n = nums.size();
-        vector<int>dp(n , -1);
-        return rob(nums , 0 , n , dp);
+        // vector<int>dp(n , -1);
+        // return rob(nums , 0 , n , dp);
+       return rob(nums , n);
     }
 };
