147632ded72fd9628f0fd03d76f4d7cf8e6aabf6
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-09-02 15:04:44 +0530

    Time: 133 ms (43.05%), Space: 43.7 MB (16.79%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
+      
+        // longest turbulent array ending at i such that arr[i-1] < arr[i]
+        // longest turbulent array ending at i such that arr[i-1] > arr[i]
+        // if(arr.size() == 0 || arr.size() == 1)return INT_MIN;
+        vector<int>dp1(arr.size() + 1 , 1);
+        vector<int>dp2(arr.size() + 1 , 1);
         
-        int n = arr.size();
- 
-        // vector<int>dp1(n , 1);
-        // longest turbulent subarray ending at i such that arr[i] > arr[i-1]
-        
-        // vector<int>dp2(n , 1);
-        // longest turbuleny subarray ending at i such that arr[i] < arr[i-1]
-        
+        dp1[0] = 1;
+        dp2[0] = 1;
         int ans = 1;
-        int pt =1, p = 1 , rt=1 , r = 1;
-        for(int i = 1 ; i < n ; i++){                 //1 1 1 1 1
-                                                     // 1 1 1 1 1
-            if(arr[i] < arr[i-1]){
-                // dp1[i] = 1 + dp2[i-1];
-                pt = 1 + r;
-            }
+        for(int i = 1 ; i < arr.size() ; i++){
             
             if(arr[i] > arr[i-1]){
-                // dp2[i] = 1 + dp1[i-1];
-                rt = 1 + p;
+                
+                dp2[i] = 1 + dp1[i-1];
+            }else if (arr[i] < arr[i-1]){
+                dp1[i] = 1 + dp2[i-1];
             }
             
-            ans = max({ans ,rt, pt});
-            // ans = max({ans , dp1[i] , dp2[i]});
-
-            p = pt; r = rt;
-                                     pt = 1 ; rt = 1;
+            ans = max({ans , dp1[i] , dp2[i]});
         }
         return ans;
         
