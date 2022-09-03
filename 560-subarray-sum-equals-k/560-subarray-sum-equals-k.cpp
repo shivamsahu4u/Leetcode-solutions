1a9303272d1f647cdb4056eadc096de12301e22d
Hritik Khurana <56023805+hritikkhurana10sm@users.noreply.github.com>
2022-09-03 15:51:41 +0530

    Time: 659 ms (5.08%), Space: 41.4 MB (77.65%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int cum=0; // cumulated sum
-        map<int,int> rec; // prefix sum recorder
-        int cnt = 0; // number of found subarray
-        rec[0]++; // to take into account those subarrays that begin with index 0
-        for(int i=0;i<nums.size();i++){
-            cum += nums[i];
-            cnt += rec[cum-k];
-            rec[cum]++;
-        }
-        return cnt;
+
+    
+         unordered_map<int,int>hash;
+         int c = 0;
+        hash[0] = 1;
+          for(int i = 0 ; i < nums.size(); i++){
+              
+              nums[i] = nums[i] + (i-1 >= 0 ?nums[i-1] : 0);
+              cout<<nums[i]<<" : "<<nums[i]-k<<" | ";
+              if(hash.find(nums[i]-k) != hash.end()){
+                  
+                  c =c + hash[nums[i]-k];
+              }
+             
+              hash[nums[i]]++;
+          }
+        
+        return c;
     }
 };
